88380dc16b9ae0cfd3cd747d81e7197146f25d5a
Your Name <your.email@example.com>
2025-04-27 20:21:00 +0200

    Add initial Python game and launch configuration for debugging

---
+++ main.py
@@ -0,0 +1,11 @@
+print('hello')
+import random
+
+liczby = input('ile liczb chcesz zgadnąć? ')
+liczby = int(liczby)
+for i in range(liczby):
+    a = random.randint(1, 10)
+    print (f'zgadnij liczbe od 1 do 10: ')
+    podana_liczba = int(input('zgadnij liczbę: '))
+    if podana_liczba == a:
+        print('zgadłeś')
