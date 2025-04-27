382eed0c3abb2d6a2249eb5f9f6a35c42ed40d0d
Your Name <your.email@example.com>
2025-04-27 21:36:17 +0200

    Refactor game logic to include input validation and error handling

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 print('hello')
 import random
 
-liczby = input('ile liczb chcesz zgadnąć? ')
-liczby = int(liczby)
-for i in range(liczby):
-    a = random.randint(1, 10)
-    print (f'zgadnij liczbe od 1 do 10: ')
-    podana_liczba = int(input('zgadnij liczbę: '))
-    if podana_liczba == a:
-        print('zgadłeś')
-pass
+try:
+    liczby = int(input('ile liczb chcesz zgadnąć? '))
+    if liczby <= 0:
+        print('Liczba prób musi być większa od 0.')
+    else:
+        for i in range(liczby):
+            a = random.randint(1, 10)
+            print(f'zgadnij liczbe od 1 do 10: ')
+            try:
+                podana_liczba = int(input('zgadnij liczbę: '))
+                if podana_liczba < 1 or podana_liczba > 10:
+                    print('Liczba musi być z przedziału od 1 do 10.')
+                if podana_liczba == a:
+                    print('zgadłeś')
+                else:
+                    print('nie zgadłeś')
+            except ValueError:
+                print('Podana wartość nie jest liczbą całkowitą! Spróbuj ponownie.')
+except ValueError:
+    print('Podana wartość nie jest liczbą całkowitą! Uruchom program ponownie i podaj poprawną liczbę.')
+    pass
 print('koniec gry')
