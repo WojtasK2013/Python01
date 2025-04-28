43d2b0bb902342c188c443e8ea4acf8e6eed81ba
Your Name <your.email@example.com>
2025-04-28 20:28:16 +0200

    dodanie kalkulatora dm

---
+++ dm_calculator.py
@@ -0,0 +1,7 @@
+print("."*150)
+print("DM Calculator")
+dm = input("podaj liczbę decmetrów kwadratowych: ")
+cm2 = float(dm) * 100
+print(f"{dm}dm2 to {cm2} cm2 ")
+mm2 = cm2 * 100
+print(f"czyli {mm2} mm2 ")
