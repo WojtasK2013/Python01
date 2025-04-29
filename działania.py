1ee01ca67686471530bbead43f435c5ae94b6a75
Your Name <your.email@example.com>
2025-04-29 18:51:19 +0200

    dziania

---
+++ działania.py
@@ -0,0 +1,15 @@
+a = (int(input('podaj pierwszą czyfrę')))
+b = (int)(input('podaj drugą czyfrę'))
+działanie = input('podaj oczekiwane działanie')
+if działanie == '-':
+  print((int(a - b)))
+if działanie == '+':
+  print((int(a + b)))
+if działanie == 'x':
+  print((int(a * b)))
+if działanie == ':':
+  print((int(a / b)))
+if działanie == 'potęga':
+  print((int(a ** b)))
+
+
