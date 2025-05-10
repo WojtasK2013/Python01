555d7f59296ca4e1ec84199592590f6b078f880e
Your Name <your.email@example.com>
2025-05-10 16:32:31 +0200

    zacząłem dokańczać projekt

--- quiz_z_angola.py
+++ quiz_z_angola.py
@@ -1,18 +1,18 @@
 print("."*150)
-ilość_pytań = input("ile chcesz pytań?")
-print("uwaga! pierwsze pytanie")
+while True:
+    if ilość_py2222222tań.isdigit():
+       ilość_pytań = (int(input("ile chcesz pytań?")))
+       break
+    else:
+        print("uwaga!zasady są proste.Musisz zamienić słówko na ekranie z polskiego na angielski")
 import random
 for i in range(int(ilość_pytań)):
- pytanie = random.randint(1, 2)
-if pytanie == 1:
-    odp = input("jak jest ''biegać,, po angielsku  ")
-if odp == 'run':
+ slowka_polskie = ['biegać','pływać','skakać','pistolet']
+slowka_ang = ['run','swim','jump','pistol']
+p = random.randint(0, len(slowka_ang)-1)
+print( slowka_polskie[p])
+odp = input("   ")
+if odp == slowka_ang[p]:
     print("dobrze")
 else:
-    print("źle")
-if pytanie == 2:
-    odp = input("jak jest ''pływać,, po angielsku  ")
-if odp == 'swim':
-    print("dobrze")
-else:
-    print("źle")
+   print(f"źle, poprawna odpowiedż to ,,{slowka_ang[p]}''")
