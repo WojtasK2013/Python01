f4ccbd33f6970bdf15c177676efe09c2fc2f5c7a
Your Name <your.email@example.com>
2025-05-13 19:45:00 +0200

    rozszerzenie słownika słówek oraz dodanie obliczania procentu poprawnych odpowiedzi

--- quiz_z_angola.py
+++ quiz_z_angola.py
@@ -3,8 +3,8 @@ punkty = 0
 ilość_pytań = int(input("ile chcesz pytań?"))
 import random
 for i in range(int(ilość_pytań)):
-    slowka_polskie = ['biegać','pływać','skakać','pistolet']
-    slowka_ang = ['run','swim','jump','pistol']
+    slowka_polskie = ['biegać','pływać','skakać','pistolet','auto','drzwi','samolot','szyny','budynek','bank']
+    slowka_ang = ['run','swim','jump','pistol','car','door','plane','rails','building','bank']
     p = random.randint(0, len(slowka_ang)-1)
     print( slowka_polskie[p])
     odp = input("   ")
@@ -14,3 +14,6 @@ for i in range(int(ilość_pytań)):
     else:
         print(f"źle, poprawna odpowiedż to ,,{slowka_ang[p]}''")
 print(f"zdobyłeś {punkty} na {ilość_pytań} punktów")
+ułamek = punkty/ ilość_pytań
+procent = ułamek * 100
+print(f"czyli {procent:.0f}% poprawnych odpowiedzi")
