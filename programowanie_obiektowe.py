82fe0814f0411123239784f7781de738f3bec461
Your Name <your.email@example.com>
2025-05-29 10:55:56 +0200

    Dodano klasę Ptaszek oraz poprawiono wywołania metod dla zwierząt

--- programowanie_obiektowe.py
+++ programowanie_obiektowe.py
@@ -9,18 +9,44 @@ class Pies(ZwierzakDomowy):
         self.rasa = rasa
     def daj_glos(self):
         print("hau hau")
+    def turlaj_się(self):
+       print('pies się turla')
 class Kot(ZwierzakDomowy):
     def __init__(self, ksywka):
         super().__init__(ksywka)
     def daj_glos(self):
         print("miau miau")
+        def turlaj_się(self):
+            print('kot się turla')
+class Ptaszek(ZwierzakDomowy):
+    def __init__(self, ksywka):
+        super().__init__(ksywka)
+    def daj_glos(self):
+        print("ćwir ćwir")
+    def turlaj_się(self):
+        print ('nie umiem się turlać')
+class Rybka(ZwierzakDomowy)
+    def __init__(self, ksywka):
+        super().__init__(ksywka)
+    def daj_glos(self):
+        print("nie umiem mówić")
+    def turlaj_się(self):
+        print ('rybka turla się pod wodą')
 pies = Pies("Pimpek","Jamnik")
+ptaszek = Ptaszek('papuga')
 złota_rybka = ZwierzakDomowy('plumek')
 kot = Kot("Mruczek")
-daj_glos = input('kto ma dać głos?')
+komenda = input('jaka   ma być komęda?')
+if komenda == 'daj głos':
+        daj_glos =  input("kto ma dać głos ")
 if daj_glos == 'pies':
     pies.daj_glos()
 if daj_glos == 'kot':
-  kot.daj_glos()
-  if daj_glos == 'ryba':
+    kot.daj_glos()
+if daj_glos == 'ryba':
     złota_rybka.daj_glos()
+if daj_glos == ptaszek:
+    ptaszek.daj_glos
+if komenda == 'turlaj się':
+ turlaj_się = input("kto ma się turlać")
+if turlaj_się == 'pies':
