dc5933a19c23c216bc8d6df3840d7d59051e6023
Your Name <your.email@example.com>
2025-06-20 13:44:46 +0200

    Refactor account management functions and update initial balance in JSON file

--- folder_bankowy/bank.py
+++ folder_bankowy/bank.py
@@ -1,13 +1,30 @@
 import json
 
-with open('folder_bankowy/rachunki_bankowe.json',encoding="utf-8",mode='r') as plik:
-    rachunki = json.load(plik)
-
+def zapisz_rachunki(rachunki):
+    with open('folder_bankowy/rachunki_bankowe.json',encoding="utf-8",mode='w') as plik:
+            plik.write(json.dumps(rachunki,indent=4))
+            print("zapisano pomyślnie")
+def odczytaj_rachunki():
+    with open('folder_bankowy/rachunki_bankowe.json',encoding="utf-8",mode='r') as plik:
+        return json.load(plik)
+rachunki = odczytaj_rachunki()
 wyszukaj_konto = input("jakie konto chcesz wyszukać?")
 if wyszukaj_konto == 'stwórz':
     nowe_konto = input('jaka ma być nazwa nowego konta')
-
-
-
-with open('folder_bankowy/rachunki_bankowe.json',encoding="utf-8",mode='w') as plik:
-    plik.write(json.dumps(rachunki,indent=4))
+   
+    rachunki[nowe_konto]={"na_koncie ":0} 
+zapisz_rachunki(rachunki)
+if wyszukaj_konto in rachunki:
+    działanie = input("jakie działanie chcesz wykonać")
+    if działanie == 'dodać':
+        ilość_pieniędzy = int(input('ile pieniędzy chcesz dodać'))
+        rachunki[wyszukaj_konto]["na_koncie"] += ilość_pieniędzy
+        print(f'{rachunki[wyszukaj_konto]}')
+        zapisz_rachunki(rachunki)   
+    elif działanie == 'odjąć':
+        ilość_pieniędzy = int(input('ile pieniędzy chcesz odjąć'))
+        rachunki[wyszukaj_konto]["na_koncie"] -= ilość_pieniędzy
+        print(f'{rachunki[wyszukaj_konto]}')
+        zapisz_rachunki(rachunki)
+    else:
+        print("nie znam działania")
